31c46d73cc2bdd51e470f952c0e83b49eea1d733
Riyan19 <rizky.riyan1978@gmail.com>
2024-04-19 09:39:43 +0700

    testimonial

--- contact.php
+++ contact.php
@@ -141,13 +141,13 @@ https://templatemo.com/tm-541-host-cloud
 
 
     <!-- Testimonials Starts Here -->
-    <!-- <div class="testimonials-section">
+    <div class="testimonials-section">
       <div class="container">
         <div class="row">
           <div class="col-md-12">
             <div class="section-heading">
               <span>Testimonials</span>
-              <h2>What they say about us</h2>
+              <h2>Apa Kata Mereka ?</h2>
             </div>
           </div>
           <div class="col-md-10 offset-md-1">
@@ -156,31 +156,31 @@ https://templatemo.com/tm-541-host-cloud
                 <div class="icon">
                   <i class="fa fa-quote-right"></i>
                 </div>
-                <p>"Ut elementum a elit sed tristique. Pellentesque sed semper erat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean quam erat, rutrum ut malesuada."</p>
-                <h4>William Smith</h4>
-                <span>New Co-Founder</span>
+                <p>"Internetnya murah banget, lancar kok enggak ada kendala."</p>
+                <h4>Nurbaiti</h4>
+                <span>Customer</span>
               </div>
               <div class="testimonial-item">
                 <div class="icon">
                   <i class="fa fa-quote-right"></i>
                 </div>
-                <p>"Quisque lacinia sed velit et maximus. Quisque dictum, lacus a malesuada finibus, arcu magna luctus risus, eu accumsan risus elit vitae lacus."</p>
-                <h4>Vinny Smart</h4>
-                <span>Digital Marketer</span>
+                <p>"Sekalipun ada kendala teknisi nya cepet banget penanganannya."</p>
+                <h4>Hendra</h4>
+                <span>Customer</span>
               </div>
               <div class="testimonial-item">
                 <div class="icon">
                   <i class="fa fa-quote-right"></i>
                 </div>
-                <p>"Vestibulum mauris ipsum, tempor tincidunt justo sit amet, bibendum tincidunt dui. Aenean molestie, odio quis viverra ultricies, leo tellus lacinia neque, sit amet maximus tortor nunc aliquet felis."</p>
-                <h4>Trevor Liam</h4>
-                <span>Technology Chef</span>
+                <p>"Ngebantu banget sih dengan harga yang murah untuk rumah-kerumah dan internetnya stabil"</p>
+                <h4>Ochin</h4>
+                <span>Customer</span>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div> -->
+    </div>
     <!-- Testimonials Ends Here -->
     
     <?php 
