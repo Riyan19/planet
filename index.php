81efc565085c58af67ed2b7c04b2f2a0284d0574
Riyan19 <rizky.riyan1978@gmail.com>
2024-04-19 09:32:53 +0700

    asd

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ https://templatemo.com/tm-541-host-cloud
         <!-- <div class="header-text"> -->
         <div class="col-6 main">
           <!-- <div class="container"> -->
-          <iframe src="https://maps.google.com/maps?q=-6.185011, 106.852203&z=15&output=embed" width="100%" height="520px" frameborder="0" style="border:0"></iframe>
+          <iframe src="https://maps.google.com/maps?q=-6.185011, 106.852203&z=15&output=embed" width="100%" height="520px" frameborder="0" style="border:0" margin-top="-100px"></iframe>
     
         </div>
         <div class="col-6">
