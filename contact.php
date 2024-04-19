d86a674cad5bdae27d8d1e06aa76de1996f67a39
Riyan19 <rizky.riyan1978@gmail.com>
2024-04-19 09:14:42 +0700

    as

--- contact.php
+++ contact.php
@@ -135,7 +135,7 @@ https://templatemo.com/tm-541-host-cloud
 -->
 
       <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11196.961132529668!2d-43.38581128725845!3d-23.011063013218724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9bdb695cd967b7%3A0x171cdd035a6a9d84!2sAv.%20L%C3%BAcio%20Costa%20-%20Barra%20da%20Tijuca%2C%20Rio%20de%20Janeiro%20-%20RJ%2C%20Brazil!5e0!3m2!1sen!2sth!4v1568649412152!5m2!1sen!2sth" width="100%" height="520px" frameborder="0" style="border:0" allowfullscreen></iframe> -->
-        <iframe src="https://maps.google.com/maps?q=-6.185011, 106.852203&z=15&output=embed" width="360" height="270" frameborder="0" style="border:0"></iframe>
+        <iframe src="https://maps.google.com/maps?q=-6.185011, 106.852203&z=15&output=embed" width="100%" height="520px" frameborder="0" style="border:0" allowfullscreen></iframe>
     </div>
     <!-- Map Ends Here -->
 
